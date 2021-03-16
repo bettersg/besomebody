48eb11dbfd12f7f9563fd48e7731e9d60ae535e4
youxuanpeng <54617188+youxuanpeng@users.noreply.github.com>
2021-03-17 01:44:55 +0800

    Create templates for Whatsapp chats

--- be-somebody/src/App.js
+++ be-somebody/src/App.js
@@ -5,6 +5,7 @@ import StoryBoard from "./Components/Boards/StoryBoard/StoryBoard";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import { blue, green, purple } from "@material-ui/core/colors";
 import Menu from "./Pages/Menu/Menu";
+import Whatsapp from "./Pages/WhatsappScreen/WhatsappScreen";
 
 const outerTheme = createMuiTheme({
   palette: {
@@ -17,6 +18,7 @@ function App() {
   return (
     <ThemeProvider theme={outerTheme}>
       <Router>
+        <Menu />
         <Switch>
           <Route exact path="/">
             <div>
@@ -25,6 +27,9 @@ function App() {
             <div>
               <Menu />
             </div>
+          </Route>
+          <Route exact path="/wa">
+            <Whatsapp />
           </Route>
         </Switch>
       </Router>
---
+++ be-somebody/src/Pages/WhatsappScreen/WhatsappScreen.js
@@ -0,0 +1,13 @@
+import React, { createContext, useState } from "react";
+import WhatsappContainer from "../../Components/Whatsapp/WhatsappContainer"
+import "./style.css";
+
+function WhatsappScreen() {
+
+    return (
+        <WhatsappContainer/>
+    );
+}
+
+
+export default WhatsappScreen;
